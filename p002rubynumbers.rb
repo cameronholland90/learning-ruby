919a72c09fe7d46e5eaf0597d676ea1bbdeff82e
Cameron Holland <cameron@thenearsky.com>
2015-10-16 13:08:12 -0500

    Working on Week 1 of ruby learning course

---
+++ p002rubynumbers.rb
@@ -0,0 +1,30 @@
+# p002rubynumbers.rb
+=begin
+	Ruby Numers
+	Usual operators
+	+ addition
+	- subtraction
+	* multiplication
+	/ division	
+=end
+
+puts 1 + 2 	# outputs 3
+puts 2 * 3	# outputs 6
+
+# Integer division
+# When you do arithmetic with integers, you'll get integer answers
+puts 3 / 2		# outputs 1
+puts 10 - 11	# outputs -1
+puts 1.5 / 2.6	# outputs 0.5769230769230769
+
+# using modulo
+puts (5 % 3)	# outputs 2
+puts (-5 % 3)	# outputs 1
+puts (5 % -3)	# outputs -1
+puts (-5 % -3)	# outputs -2
+
+# or and || operator
+puts nil || 2008	# outputs 2008
+puts false || 2008	# outputs 2008
+puts "ruby" || 2008	# outputs ruby
+
---
+++ p003rubystrings.rb
@@ -0,0 +1,24 @@
+# p003rubystrings.rb
+=begin
+
+	Ruby String
+	In Ruby, strings are mutable
+	
+=end
+
+# Can use " or ' for Strings, but ' is more efficient
+puts "Hello World"
+puts 'Hello World'
+
+# String concatenation
+puts 'I like' + ' Ruby'
+
+# Escape sequence
+puts 'It\'s my Ruby'
+
+# New here, displays the string three times
+puts 'Hello' * 3
+
+# Defining a constant
+PI = 3.1416
+puts PI
