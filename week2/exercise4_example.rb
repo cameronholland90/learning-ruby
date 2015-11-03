32e2f10c7fa1d32ce8f717ccb18dbf9b2739c405
Cameron Holland <cameron@thenearsky.com>
2015-11-03 15:19:54 -0600

    week 2 exercise 4-6 THIMK code examples

---
+++ week2/exercise4_example.rb
@@ -0,0 +1,23 @@
+# example a
+puts expand = defined?( expand ) ? expand : true
+
+# example b
+x = 10
+
+y = 20
+
+x, y = y, x
+
+puts x
+
+puts y
+
+# example c
+if false
+
+	x = pqr
+end
+
+puts x
+
+puts pqr
